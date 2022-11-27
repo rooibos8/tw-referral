71f9e1fe9dcb140c0d65cda6bd970c4869876744
mayo0828 <nakamura_mayumi@mayo0828.com>
2022-11-27 01:36:26 -0800

    fixed typeerror

--- src/components/icons/TwitterIcon.tsx
+++ src/components/icons/TwitterIcon.tsx
@@ -1,13 +1,10 @@
 import { Twitter } from '@mui/icons-material';
+import { SvgIconProps } from '@mui/material';
 import React from 'react';
 
 import variables from '@/styles/variables.module.scss';
 
 // @ts-ignore
-export const TwitterIcon = ({ classes, ...props }) => (
-  <Twitter
-    style={{ color: variables.twitterColor }}
-    classes={classes}
-    {...props}
-  />
+export const TwitterIcon: React.FC<SvgIconProps> = ({ ...props }) => (
+  <Twitter style={{ color: variables.twitterColor }} {...props} />
 );
